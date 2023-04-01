632525c204d94ff0825624e04d7c4dfc7b037ad2
Tiago Simões <tiago.simoes@uni-foundation.eu>
2023-04-01 23:28:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# VM tools
+
+Simple wrapper around **Vagrant** that sets `$VAGRANT_HOME` to a directory other than `~/.vagrant.d`, as well as the VirtualBox `machinefolder` to a directory other than the default `~/VirtualBox VMs`. The script then executes the `vagrant` command and resets the values to their defaults.
+
+This approach allows VMs to be stored in a removable device.
+
+## Requirements
+
+This script requires **Vagrant** and **VirtualBox** to be installed.
+
+## Installation
+
+## Installation
+
+First grab the script and have a look at what is included:
+
+    git clone https://github.com/tiagosimoes-euf/vmtools.git
+    cd vmtools/ && ls -hAl
+
+Copy the example config file and edit the active config file:
+
+    cp example.vm.cfg vm.cfg
+    nano vm.cfg
+
+Make the script executable and put it somewhere in your `$PATH`:
+
+    chmod a+x vm.sh
+    sudo ln -s ${PWD}/vm.sh /usr/local/bin/vm
+
+## Usage
+
+Run `vm global-status` and see the output of `vagrant global-status`.
---
+++ vm.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+# Set the main variables.
+SCRIPTPATH=$(dirname $(realpath $0))
+CONFIGEXAMPLE='example.vm.cfg'
+CONFIGFILE='vm.cfg'
+
+# Assert a configuration file.
+if ! [ -f ${SCRIPTPATH}/${CONFIGFILE} ]; then
+  cp ${SCRIPTPATH}/${CONFIGEXAMPLE} ${SCRIPTPATH}/${CONFIGFILE}
+fi
+
+# Import configuration.
+source ${SCRIPTPATH}/${CONFIGFILE}
+
+# Check requirements and set new variables.
+if ! [ -z ${VAGRANT_HOME_EXTERNAL} ]; then
+  if ! [ -x "$(command -v vagrant)" ]; then
+    echo 'Error: vagrant is not installed.'
+    exit 1
+  fi
+
+  if ! [ -d ${VAGRANT_HOME_EXTERNAL} ]; then
+    echo "Error: ${VAGRANT_HOME_EXTERNAL} is not accessible."
+    exit 1
+  fi
+
+  # Record preset environment variables.
+  VAGRANT_HOME_PRESET=${VAGRANT_HOME}
+
+  # Set new environment variables.
+  export VAGRANT_HOME=${VAGRANT_HOME_EXTERNAL}
+fi
+
+if ! [ -z ${VBOX_FOLDER_EXTERNAL} ]; then
+  if ! [ -x "$(command -v vboxmanage)" ]; then
+    echo 'Error: vboxmanage is not installed.'
+    exit 1
+  fi
+
+  if ! [ -d ${VBOX_FOLDER_EXTERNAL} ]; then
+    echo "Error: ${VBOX_FOLDER_EXTERNAL} is not accessible."
+    exit 1
+  fi
+
+  vboxmanage setproperty machinefolder ${VBOX_FOLDER_EXTERNAL}
+fi
+
+# Extract arguments as an array
+ARGS=("$@")
+VAGRANT_COMMAND="${ARGS[0]}"
+
+# Require confirmation for certain vagrant commands
+case ${VAGRANT_COMMAND} in
+  "destroy" )
+  echo "WARNING: this might be a bad idea!"
+  ;;
+esac
+
+# Run 'vagrant' command with all arguments
+echo "vagrant $@"
+vagrant $@
+
+# Reset environment variables.
+if ! [ -z ${VAGRANT_HOME_EXTERNAL} ]; then
+  if ! [ -z ${VAGRANT_HOME_PRESET} ]; then
+    export VAGRANT_HOME=${VAGRANT_HOME_PRESET}
+  else
+    unset VAGRANT_HOME
+  fi
+fi
+
+if ! [ -z ${VBOX_FOLDER_EXTERNAL} ]; then
+  vboxmanage setproperty machinefolder default
+fi
+
+exit 0
